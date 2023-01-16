ea6ad7661644c9c900024426711d3654bde081e9
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2023-01-17 00:36:56 +0300

    Sign up front success

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { HomepageComponent } from './pages/homepage/homepage.component';
 import { LoginComponent } from './pages/login/login.component';
 import { StatRepairComponent } from './components/stat-repair/stat-repair.component';
 import { ListRepairComponent } from './components/list-repair/list-repair.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'signUp', component: SignupComponent },
   { path: 'listRepair', component: ListRepairComponent },
   { path: 'statRepair', component: StatRepairComponent },
 ];
