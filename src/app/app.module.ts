3b1b208b19e36ccaa28324ddef98e80fd29ba21f
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2023-01-23 15:51:43 +0300

    success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainWindowComponent } from './components/main-window/main-window.component';
@@ -10,16 +10,18 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { StatRepairComponent } from './components/stat-repair/stat-repair.component';
 import { ListRepairComponent } from './components/list-repair/list-repair.component';
+import { SignupComponent } from './pages/signup/signup.component';
 @NgModule({
   declarations: [
     AppComponent,
     MainWindowComponent,
     HomepageComponent,
     LoginComponent,
+    SignupComponent,
     StatRepairComponent,
     ListRepairComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
